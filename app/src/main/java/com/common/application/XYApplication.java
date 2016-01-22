4d6d381c3829ac033f6a7620fded00180ac50990
huangli <2455707015@qq.com>
2016-01-22 19:09:36 +0800

    框架搭建

---
+++ app/src/main/java/com/common/application/XYApplication.java
@@ -0,0 +1,10 @@
+package com.common.application;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by 黄笠 on 2016/1/22.
+ */
+public class XYApplication extends MultiDexApplication {
+
+}
