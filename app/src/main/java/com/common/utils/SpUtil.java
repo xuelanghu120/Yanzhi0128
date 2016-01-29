bc5f70e34267f956176412e38915ac4c427e9cd9
liang <564170728@qq.com>
2016-01-29 10:59:18 +0800

    hu

--- app/src/main/java/com/common/utils/SpUtil.java
+++ app/src/main/java/com/common/utils/SpUtil.java
@@ -14,13 +14,28 @@ public class SpUtil {
     public static final String KEY_TOKEN = "key_token";
     public static final String KEY_NICKNAME = "key_nickname";
 
-
+    //存放user的昵称和token
     public static void saveLogin(User entity){
         SharedPreferences sp = Global.mContext.getSharedPreferences(XYConfig.PREFERENCES_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putString(KEY_TOKEN, entity.token);
         editor.putString(KEY_NICKNAME, entity.nickname);
     }
+    //获取token
+    public static String getToken(){
+        SharedPreferences sp = Global.mContext.getSharedPreferences(XYConfig.PREFERENCES_NAME, Context.MODE_PRIVATE);
+        return sp.getString(KEY_TOKEN,"");
+    }
+    //获取user
+    public static User getUser(){
+        SharedPreferences sp = Global.mContext.getSharedPreferences(XYConfig.PREFERENCES_NAME, Context.MODE_PRIVATE);
+        User user = new User();
+        user.token = sp.getString(KEY_TOKEN,"");
+        user.nickname = sp.getString(KEY_NICKNAME,"");
+        return user;
+
+    }
+
     public static void clearLoginInfo() {
         SharedPreferences sp = Global.getContext().getSharedPreferences(XYConfig.PREFERENCES_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
