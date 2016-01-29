9c5e0be9a5064cd11dd689564a8b817a4f4d02dc
liang <564170728@qq.com>
2016-01-29 17:27:41 +0800

    hu  友盟

--- app/src/main/java/com/common/base/activity/BaseActivity.java
+++ app/src/main/java/com/common/base/activity/BaseActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 import com.common.application.XYApplication;
+import com.common.common.Global;
+import com.umeng.analytics.MobclickAgent;
 
 /**
  * Created by 黄笠 on 2016/1/22.
@@ -38,11 +40,21 @@ public abstract class BaseActivity extends Activity{
 
     @Override
     protected void onResume() {
+        Global.isPageFont =true;
+        MobclickAgent.onResume(this);//友盟统计
+        MobclickAgent.onPageStart("SplashScreen"); //统计页面(仅有Activity的应用中SDK自动调用，不需要单独写。"SplashScreen"为页面名称，可自定义)
+
+//        MobclickAgent.onEvent(Context context, String eventId);
+//        context 指当前的Activity
+//        eventId 为当前统计的事件ID。
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+        Global.isPageFont =false;
+        MobclickAgent.onPause(this);//友盟统计
+        MobclickAgent.onPageEnd("SplashScreen"); // （仅有Activity的应用中SDK自动调用，不需要单独写）保证 onPageEnd 在onPause 之前调用,因为 onPause 中会保存信息。"SplashScreen"为页面名称，可自定义
         super.onPause();
     }
 
