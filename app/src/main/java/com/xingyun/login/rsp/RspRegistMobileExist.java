179adde0372928e163c580e7b7b6d11421d415fd
liang <564170728@qq.com>
2016-02-17 11:07:44 +0800

    注册返回

--- app/src/main/java/com/xingyun/login/rsp/RspRegistMobileExist.java
+++ app/src/main/java/com/xingyun/login/rsp/RspRegistMobileExist.java
@@ -1,6 +1,11 @@
 package com.xingyun.login.rsp;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.common.common.Global;
 import com.common.http.YanzhiAbstractResponser;
+import com.common.http.base.RspBaseEntity;
 import com.google.gson.Gson;
 import com.xingyun.login.model.entity.ResultData;
 
@@ -11,10 +16,18 @@ import org.json.JSONObject;
  */
 public class RspRegistMobileExist extends YanzhiAbstractResponser {
 
-    public ResultData<Integer> result;
+    public ResultData result;
 
     @Override
-    public void parserResult(JSONObject jsonObject, Gson gson) {
-        result = gson.fromJson(jsonObject.toString(),ResultData.class);
+    public void parserResult(int code, JSONObject jsonObject, Gson gson) {
+        if (null!=jsonObject && code==0){
+            result = gson.fromJson(jsonObject.toString(),ResultData.class);
+        }else {
+            RspBaseEntity entity = gson.fromJson(jsonObject.toString(),RspBaseEntity.class);
+            isSuccess =false;
+            errorCode = entity.code;
+            errorMessage = entity.desc;
+            result = null;
+        }
     }
 }
